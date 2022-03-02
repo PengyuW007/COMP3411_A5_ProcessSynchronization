d1c12191d82ff939cf719fcd2519221f21f3c1a6
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-03-02 09:36:18 -0600

    Getters added

--- src/ProducerConsumer_Process_Synchronization.java
+++ src/ProducerConsumer_Process_Synchronization.java
@@ -1,2 +1,35 @@
 public class ProducerConsumer_Process_Synchronization {
+    private final int SIZE = 5;
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Buffer{
+    private int[]buffer;
+    private int write;//producer
+    private int read;//consumer
+
+    public Buffer(int size){
+        buffer = new int[size];
+        write = 0;
+        read = 0;
+    }
+
+
+    /*
+    Getters
+     */
+    public int[] getBuffer() {
+        return buffer;
+    }
+
+    public int getWrite(){
+        return write;
+    }
+
+    public int getRead() {
+        return read;
+    }
 }
