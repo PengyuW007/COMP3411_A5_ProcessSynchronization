12b8ee955d9d7c2d8d98232f608dac47da0be2bb
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-03-04 12:00:43 -0600

    Update ProducerConsumer_Process_Synchronization.java

--- src/ProducerConsumer_Process_Synchronization.java
+++ src/ProducerConsumer_Process_Synchronization.java
@@ -6,17 +6,54 @@ public class ProducerConsumer_Process_Synchronization {
     }
 }
 
-class Buffer{
-    private int[]buffer;
+class Buffer {
+    private int[] buffer;
     private int write;//producer
     private int read;//consumer
+    private int count;//number of items in buffer
+    //private boolean available;//available to read/write, AKA empty or full
 
-    public Buffer(int size){
+    public Buffer(int size) {
         buffer = new int[size];
         write = 0;
         read = 0;
+        count = 0;
     }
 
+    public synchronized int put(int value) {
+        int size = buffer.length;
+        while (count == size) {
+            try {
+                wait();
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+        buffer[write] = value;
+        write = (write + 1) % size;
+        count++;
+        System.out.println("Produced " + value + "," + count + " items in Buffer.");
+        notifyAll();
+        return count;
+    }//end put
+
+    public synchronized int withdraw() {
+        int size = buffer.length;
+        while (count == 0) {
+            try {
+                wait();
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+
+        int next_consumed = buffer[read];
+        read = (read + 1) % size;
+        count--;
+        System.out.println("Consumed " + next_consumed + "," + count + " items in Buffer.");
+        notifyAll();
+        return next_consumed;
+    }//end withdraw
 
     /*
     Getters
@@ -25,11 +62,15 @@ class Buffer{
         return buffer;
     }
 
-    public int getWrite(){
+    public int getWrite() {
         return write;
     }
 
     public int getRead() {
         return read;
     }
-}
+
+    public int counter() {
+        return count;
+    }
+}//end Buffer
